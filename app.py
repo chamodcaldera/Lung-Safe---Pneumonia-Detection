540ee20482fea682ee063fb8fe4bb64db6960f66
Chamod Caldera <chamod.20200747@iit.ac.lk>
2024-03-29 22:50:45 +0530

    segmentation function added.

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
+from Segmentation.Segmentation import seg_predict
+from PIL import Image
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('nh.html')
+
+
+@app.route("/predict", methods=["POST"])
+def predict_seg():
+    if "image" not in request.files:
+        return "No image file in request", 400
+
+    file = request.files["image"]
+    image = Image.open(file.stream)  # Read the image in PIL format
+    img_str = seg_predict(image)
+    img_str = img_str.decode('utf-8')
+    return render_template('nh.html', img_str=img_str)
 
 
 if __name__ == '__main__':
